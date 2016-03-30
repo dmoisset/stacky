c188a289e62eb9a15f13989b163581d887eba7c6
Daniel F Moisset <dmoisset@machinalis.com>
2016-03-30 20:37:52 +0100

    Fixes in names for operators

--- opcodes.py
+++ opcodes.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 Opcode = Enum('Opcode', 'push oper let get print')
 
-Operator = Enum('Operator', 'plus minus mul div neg lt gt eq ne le ge and_ or_ not_')
+Operator = Enum('Operator', 'plus minus mul div lt gt eq ne le ge and_ or_ not_')
 
 # program = [(Opcode.get, "x"), (Opcode.get, "y"), (Opcode.oper, Operator.plus)]
