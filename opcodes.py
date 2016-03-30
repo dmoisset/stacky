db614cbfcfd9d8b757505ae4d8954a06e91618c6
Daniel F Moisset <dmoisset@machinalis.com>
2016-03-30 20:21:00 +0100

    Added opcodes for other operators

--- opcodes.py
+++ opcodes.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 Opcode = Enum('Opcode', 'push oper let get print')
 
-Operator = Enum('Operator', 'plus minus mul div')
+Operator = Enum('Operator', 'plus minus mul div neg lt gt eq ne le ge and or not')
 
 # program = [(Opcode.get, "x"), (Opcode.get, "y"), (Opcode.oper, Operator.plus)]
